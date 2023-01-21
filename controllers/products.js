a27de678c5e169acae47241fe1e4bbad7cfea12a
bulat <bulatxajrutdinoff@yandex.ru>
2023-01-21 15:51:00 +0300

    mvc: models

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,19 @@
 const adminData = require("../routes/admin");
-const products = []
+const Product = require('../models/product')
 
 exports.actionGetAddProduct = (req, res) => {
     res.render('add-product', { title: 'Add Product', path: '/admin/add-product' })
 }
 
 exports.actionPostAddProduct = (req, res) => {
-    products.push({ title: req.body.title })
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/')
 }
 
 exports.actionGetProducts = (req, res, next) => {
     // uses default templating engine, which we defined in server.js
-    res.render('shop', { products, title: 'Shop', path: '/' })
+    res.render('shop', { products: Product.fetchAll(), title: 'Shop', path: '/' })
 }
 
 // exports.products = products
