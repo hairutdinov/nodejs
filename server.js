85aea29cef89bc3ae2423aca07aa3f8f993f55a5
bulat <bulatxajrutdinoff@yandex.ru>
2023-01-21 12:48:06 +0300

    handlebars - not finished

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const path = require('path')
 const express = require('express')
 const rootDir = require('./helpers/path')
 const bodyParser = require('body-parser')
+const handlebars = require('express-handlebars');
 
 const app = express()
 
-app.set('view engine', 'pug')
-// where to find pug template
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+}));
+// where to find handlebars template
 app.set('views', 'views')
 
 const adminData = require('./routes/admin')
@@ -21,7 +27,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoute)
 
 app.use((req, res, next) => {
-    res.status(404).render('404-not-found', { title: '404 Page Not Found' })
+    res.status(404).render('404-not-found', { title: '404 Page Not Found' });
 })
 
 app.listen(8101)
