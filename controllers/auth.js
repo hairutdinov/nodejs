1bce09a44d9a26239f8608aad78c8df8d7218245
bulat <bulatxajrutdinoff@yandex.ru>
2023-01-25 17:27:46 +0300

    cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,17 @@
-
-
-exports.getAuth = (req, res, next) => {
+exports.getLogin = (req, res, next) => {
+    const loggedInCookie = req.get('Cookie').split(';').find(cookie => cookie.indexOf('loggedIn') !== -1);
+    let loggedIn = false
+    if (loggedInCookie) {
+        loggedIn = (loggedInCookie.trim().split('=')[1] ?? false) === 'true'
+    }
     res.render('auth/login', {
         path: '/auth/login',
-        title: 'Login'
+        title: 'Login',
+        isAuthenticated: loggedIn
     })
+}
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+    res.redirect('/')
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const authController = require('../controllers/auth')
 
 
-router.get('/login', authController.getAuth)
+router.get('/login', authController.getLogin)
+
+router.post('/login', authController.postLogin)
 
 module.exports = router
