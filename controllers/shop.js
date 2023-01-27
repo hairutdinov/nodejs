3f7c5fa47581879ede3cc5de16cb4f75d995140b
bulat <bulatxajrutdinoff@yandex.ru>
2023-01-27 21:25:13 +0300

    pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,10 +5,31 @@ const fs = require('fs')
 const path = require('path')
 const PDFDocument = require('pdfkit')
 
+const ITEMS_PER_PAGE = 1
+
 exports.getIndex = async (req, res) => {
+    const page = +req.query.page || 1
+    let totalItems
     Product.find()
+        .countDocuments()
+        .then(count => {
+            totalItems = count
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
-            res.render('shop/index', { products, title: 'Shop', path: '/' })
+            res.render('shop/index', {
+                products,
+                title: 'Shop',
+                path: '/',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+            })
         })
         .catch(e => {
             console.error(e)
